af32af16b950f1bc031d7d7f6e5b06a4cea561d7
Nirav Dholiya <140297412+nirav07dholiya@users.noreply.github.com>
2025-08-19 11:02:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,6 +27,7 @@ app.use(
   })
 );
 
+app.use('/uploads', express.static('uploads'));
 app.use("/uploads/profiles", express.static("uploads/profiles"));
 app.use("/uploads/posts", express.static("uploads/posts"));
 
